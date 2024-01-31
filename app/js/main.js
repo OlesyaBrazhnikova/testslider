f67ccd96b73362b0bf15e50f92762eb9b8c235af
Olesya <olesyabrazhnikova@ukr.net>
2024-01-31 12:38:38 +0200

    example

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,22 @@ var swiper = new Swiper('.swiper', {
 		}
 	  }
 });
+
+const palindrome = (str) => {
+	const string = str.toLowerCase().replace(/[^0-9a-z]/gi, '').split('');
+	for (let i = 0; i < string.length/2; i++) {
+		if (string[i] !== string[string.length -1 -i]) {  
+		return false;
+		} 
+	}
+	return true;
+}
+console.log(palindrome('_level'));
+
+
+const multiplyAll = arr => arr.reduce((a,b)=> a*b,1);
+console.log(multiplyAll([1,2,3,4]));
+
+
+const wordsToMarks = string => [...string].reduce((res, c) => res += c.charCodeAt() - 96, 0)
+console.log(wordsToMarks('hello'));
